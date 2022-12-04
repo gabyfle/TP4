4cdfd7d483ca325692ab32eb9d4df9fcccaf1401
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2022-12-04 14:24:49 +0100

    Adding first files to TP4

---
+++ build.bash
@@ -0,0 +1,16 @@
+# Building assembly using as Assembler with -g flag
+
+# Creating build directory if not exists
+mkdir -p build
+
+if [[ -z $1 && -z $2 ]]; then
+    echo "Usage: build.bash <source file> <output file>"
+    exit 1
+fi
+# Assembling the file name given in argument $1
+as -g $1 -o build/$1.o
+# Linking the object file
+gcc build/$1.o -o build/$2 -nostdlib
+
+# Deleting object files
+rm build/$1.o
